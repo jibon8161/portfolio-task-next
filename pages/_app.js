0d558995e3728db0d264296ac9b54c333d4d51ab
jibon8161 <jibon2230@gmail.com>
2024-03-19 02:25:23 +0600

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
 import "@/styles/globals.css";
+import "../public/assets/css/all.css";
+import "../public/assets/css/bootstrap.min.css";
+import "../public/assets/css/default.css";
+import "../public/assets/css/magnific-popup.css";
+import "../public/assets/css/responsive.css";
+import "../public/assets/css/slick-menu.css";
+import "../public/assets/css/slick.css";
+import "../public/assets/css/style.css";
+// import "../public/assets/css/fontawesome-all.min.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
---
+++ pages/api/Service.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const Service = () => {
+    return (
+      <div>
+        <section id="s" className="service-area service-categories pt-100">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-4">
+                <div className="single-service-area">
+                  <span className="service-num">01</span>
+                </div>
+                <div className="service-icon">
+                  <img src="assets/images/service-icon/1.png" alt="" />
+                </div>
+                <div className="service-content">
+                  <h4>Product Design</h4>
+                  <p>
+                    It is pleasure, but because those who do not know how pursue
+                    pleasure ration
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="single-service-area">
+                  <span className="service-num">02</span>
+                </div>
+                <div className="service-icon">
+                  <img src="assets/images/service-icon/2.png" alt="" />
+                </div>
+                <div className="service-content">
+                  <h4>UX/UI Strategy</h4>
+                  <p>
+                    Pain can procure hi some great pleasutrivial example, which
+                    ever undertakes{" "}
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="single-service-area">
+                  <span className="service-num">03</span>
+                </div>
+                <div className="service-icon">
+                  <img src="assets/images/service-icon/3.png" alt="" />
+                </div>
+                <div className="service-content">
+                  <h4>Market Analysis</h4>
+                  <p>
+                    Some advantage from but who has any right tfind fault with a
+                    man chooses
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+};
+
+export default Service;
